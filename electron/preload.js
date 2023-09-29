e7babefb3f6eeb398f0ff16b2ef6a82c9a0c745a
ricmsd <ricmsd@gmail.com>
2023-09-29 16:04:34 +0900

    Gaps between containers were erased.

--- electron/preload.js
+++ electron/preload.js
@@ -2,7 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
   pathToFileURL: (path) => {
-    console.log('path', path);
     return ipcRenderer.invoke('node:pathToFileURL', path);
   }
 });
--- player/src/app/app.component.ts
+++ player/src/app/app.component.ts
@@ -23,13 +23,33 @@ export class AppComponent implements OnInit {
   private readonly VIDEO_CONTAINER_COUNT_MIN = 1;
   private readonly VIDEO_CONTAINER_COUNT_MAX = 10;
   private readonly VIDEO_CONTAINER_COUNT_STEP = 1;
-  public videoContainerWidth?: string;
 
-  public videoFiles: VideoFile[] = [
-  ]
+  public clientWidth?: number;
+  public videoContainerWidth?: string;
+  public videoContainerHeight?: string;
+  public videoWidth?: string;
+  public videoHeight?: string;
+
+  public videoFiles: VideoFile[] = [];
+  public getAllRowNumberArray(): number[] {
+    const nRow = Math.ceil(this.videoFiles.length / this.videoContainerCount);
+    const result: number[] = [];
+    for (let i = 0; i < nRow; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+  public getVideoFiles(row: number): VideoFile[] {
+    const start = row * this.videoContainerCount;
+    const end = start + this.videoContainerCount;
+    return this.videoFiles.slice(start, end);
+  }
 
   constructor(private ngZone: NgZone) {
     this.updateVideoContainerWidth();
+    for (let i = 0; i < 10; i++) {
+      this.addNewVideoFile('file://c:/workspace/tmp/test.mp4');
+    }
   }
 
   ngOnInit(): void {
@@ -57,13 +77,11 @@ export class AppComponent implements OnInit {
     for (let i = 0; i < (event.dataTransfer?.files?.length||0); i++) {
       const file: File = <File>event.dataTransfer?.files[i];
       const path = (<any>file).path;
-      console.log(path, typeof path);
       if (typeChecker.canPlayType(file.type) === '') {
         console.info(`can't play this file: ${path}`);
         continue;
       }
       const url = await window.electronAPI.pathToFileURL(path);
-      console.log(url);
       this.addNewVideoFile(url);
     }
   }
@@ -88,7 +106,36 @@ export class AppComponent implements OnInit {
   }
 
   private updateVideoContainerWidth(): void {
-    this.videoContainerWidth = (document.body.clientWidth / this.videoContainerCount - 1) + 'px';
+    this.clientWidth = document.body.clientWidth;
+    const width = this.clientWidth / this.videoContainerCount;
+    const height = (width / 16) * 9;
+
+    // To be an integer value for accurate drawing.
+    this.videoContainerWidth = Math.floor(width) + 'px';
+    this.videoContainerHeight = Math.floor(height) + 'px';
+
+    // To fill the 1px gap between containers
+    // (Cause of the gap is unknown)
+    this.videoWidth = Math.floor(width + 1) + 'px';
+    this.videoHeight = Math.floor(height + 1) + 'px';
+  }
+  public getVideoContainerWidth(isLast: boolean): string {
+    if (!isLast) {
+      return <string>this.videoContainerWidth;
+    }
+
+    // Add margins to the last container.
+    const width = Math.floor(<number>this.clientWidth / this.videoContainerCount);
+    return (width + (<number>this.clientWidth - width * this.videoContainerCount)) + 'px';
+  }
+  public getVideoWidth(isLast: boolean): string {
+    if (!isLast) {
+      return <string>this.videoWidth;
+    }
+
+    // Add margins to the last container.
+    const width = Math.floor(<number>this.clientWidth / this.videoContainerCount);
+    return (width + (<number>this.clientWidth - width * this.videoContainerCount) + 1) + 'px';
   }
 
   public onLoadedMetadata(video: HTMLVideoElement, videoFile: VideoFile): void {
@@ -104,8 +151,8 @@ export class AppComponent implements OnInit {
   public onTimeSlideEnd(slider: Slider, video: HTMLVideoElement, miniVideo: HTMLVideoElement, videoFile: VideoFile): void {
     video.currentTime = videoFile.currentTime;
   }
-  public onVideoTimeUpdate(slider: Slider, video: HTMLVideoElement, videoFile: VideoFile): void {
-    if (slider.dragging) {
+  public onVideoTimeUpdate(slider: Slider | null, video: HTMLVideoElement, videoFile: VideoFile): void {
+    if (slider?.dragging) {
       return;
     }
     videoFile.currentTime = video.currentTime;
